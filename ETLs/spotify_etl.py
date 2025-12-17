1e1ab4fdc1795d28bde012d0fea3028c13f288fa
LLWICK <linwick679@gmail.com>
2025-12-17 12:40:13 +0530

    airflow setup

--- ETLs/spotify_etl.py
+++ ETLs/spotify_etl.py
@@ -18,16 +18,30 @@ CLIENT_SECRET = os.getenv("SPOTIFY_SECRET")
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-tracks = sp.artist_top_tracks("0Y5tJX1MQlPlqiwlOH1tJY")  
+ARTIST_ID = "0Y5tJX1MQlPlqiwlOH1tJY"  # Travis Scott
 
-rows = []
-for track in tracks["tracks"]:
-    rows.append({
-        "track_name": track["name"],
-        "popularity": track["popularity"],
-        "duration_ms": track["duration_ms"],
-        "release_date": track["album"]["release_date"]
-    })
+albums = sp.artist_albums(ARTIST_ID, album_type="album", limit=50)
+album_ids = [a["id"] for a in albums["items"]]
 
-df = pd.DataFrame(rows)
-df.to_csv("./data/travisScott_top_tracks.csv", index=False)
+track_ids = []
+for album_id in album_ids:
+    tracks = sp.album_tracks(album_id)
+    for track in tracks["items"]:
+        track_ids.append(track["id"])
+
+track_ids = list(set(track_ids))  # remove duplicates
+
+tracks_data = []
+for i in range(0, len(track_ids), 50):
+    batch = sp.tracks(track_ids[i:i+50])
+    for t in batch["tracks"]:
+        tracks_data.append({
+            "track_name": t["name"],
+            "popularity": t["popularity"],
+            "album": t["album"]["name"]
+        })
+
+df = pd.DataFrame(tracks_data)
+top_100 = df.sort_values("popularity", ascending=False).head(100)
+
+top_100.to_csv("travis_scott_top_100.csv", index=False)
