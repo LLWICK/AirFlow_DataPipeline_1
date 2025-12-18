91231ab9a1f557a1aeb415e6fbdc84129f28bd1a
LLWICK <linwick679@gmail.com>
2025-12-18 15:51:55 +0530

    update spotify creds

--- DAGs/spotify_etl.py
+++ DAGs/spotify_etl.py
@@ -1,16 +1,21 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import json
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 import pandas as pd
 
 
 def run_spotify_etl():
 
-  
+    BASE_DIR = Path(__file__).resolve().parent
+    ENV_PATH = BASE_DIR.parent / ".env"
+    load_dotenv(ENV_PATH)
 
     # Your credentials
-    CLIENT_ID = "85d5477d189a4f04ab629510143a2c5b"
-    CLIENT_SECRET = "5fada1c4653545a8883de92c99bfa522"
+    CLIENT_ID = os.getenv("SPOTIFY_ID")
+    CLIENT_SECRET = os.getenv("SPOTIFY_SECRET")
 
     # Set up Spotipy client with client credentials flow
     client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
