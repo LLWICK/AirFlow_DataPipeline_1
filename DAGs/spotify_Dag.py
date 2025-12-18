3f818ee234b2f8ba65c2396248265b9d96af0d25
LLWICK <linwick679@gmail.com>
2025-12-18 08:38:26 +0530

    create spotify DAG

---
+++ DAGs/spotify_Dag.py
@@ -0,0 +1,38 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime 
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
+from ETLs.spotify_etl import run_spotify_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 12, 18),
+    'email':['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry':False,
+    'retries':1,
+    'retry_delay':timedelta(minutes=1)
+}
+
+dag = DAG(
+    'spotify_dag',
+    default_args=default_args,
+    description='My first ETL'
+)
+
+run_etl = PythonOperator(
+    task_id = 'complete_spotify_etl',
+    python_callable= run_spotify_etl,
+    dag = dag,
+)
+
+run_etl
--- ETLs/spotify_etl.py
+++ ETLs/spotify_etl.py
@@ -6,42 +6,45 @@ import os
 from pathlib import Path
 import pandas as pd
 
-BASE_DIR = Path(__file__).resolve().parent
-ENV_PATH = BASE_DIR.parent / ".env"
-load_dotenv(ENV_PATH)
 
-# Your credentials
-CLIENT_ID = os.getenv("SPOTIFY_ID")
-CLIENT_SECRET = os.getenv("SPOTIFY_SECRET")
+def run_spotify_etl():
 
-# Set up Spotipy client with client credentials flow
-client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    BASE_DIR = Path(__file__).resolve().parent
+    ENV_PATH = BASE_DIR.parent / ".env"
+    load_dotenv(ENV_PATH)
 
-ARTIST_ID = "0Y5tJX1MQlPlqiwlOH1tJY"  # Travis Scott
+    # Your credentials
+    CLIENT_ID = os.getenv("SPOTIFY_ID")
+    CLIENT_SECRET = os.getenv("SPOTIFY_SECRET")
 
-albums = sp.artist_albums(ARTIST_ID, album_type="album", limit=50)
-album_ids = [a["id"] for a in albums["items"]]
+    # Set up Spotipy client with client credentials flow
+    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-track_ids = []
-for album_id in album_ids:
-    tracks = sp.album_tracks(album_id)
-    for track in tracks["items"]:
-        track_ids.append(track["id"])
+    ARTIST_ID = "0Y5tJX1MQlPlqiwlOH1tJY"  # Travis Scott
 
-track_ids = list(set(track_ids))  # remove duplicates
+    albums = sp.artist_albums(ARTIST_ID, album_type="album", limit=50)
+    album_ids = [a["id"] for a in albums["items"]]
 
-tracks_data = []
-for i in range(0, len(track_ids), 50):
-    batch = sp.tracks(track_ids[i:i+50])
-    for t in batch["tracks"]:
-        tracks_data.append({
-            "track_name": t["name"],
-            "popularity": t["popularity"],
-            "album": t["album"]["name"]
-        })
+    track_ids = []
+    for album_id in album_ids:
+        tracks = sp.album_tracks(album_id)
+        for track in tracks["items"]:
+            track_ids.append(track["id"])
 
-df = pd.DataFrame(tracks_data)
-top_100 = df.sort_values("popularity", ascending=False).head(100)
+    track_ids = list(set(track_ids))  # remove duplicates
 
-top_100.to_csv("travis_scott_top_100.csv", index=False)
+    tracks_data = []
+    for i in range(0, len(track_ids), 50):
+        batch = sp.tracks(track_ids[i:i+50])
+        for t in batch["tracks"]:
+            tracks_data.append({
+                "track_name": t["name"],
+                "popularity": t["popularity"],
+                "album": t["album"]["name"]
+            })
+
+    df = pd.DataFrame(tracks_data)
+    top_100 = df.sort_values("popularity", ascending=False).head(100)
+
+    top_100.to_csv("travis_scott_top_100.csv", index=False)
